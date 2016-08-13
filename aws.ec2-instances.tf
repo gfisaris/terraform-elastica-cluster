19c11c17558aacf0508b80625d14de2974dea779
Georgios Fisaris <gfisaris@gmail.com>
2016-08-13 20:35:58 +0100

    Rename Resource and add a Name Tag

--- aws.ec2-instances.tf
+++ aws.ec2-instances.tf
@@ -1,4 +1,8 @@
-resource "aws_instance" "trfm-instance-X" {
+resource "aws_instance" "terraform-ec2_instance-X" {
 	ami			= "ami-7abd0209"
 	instance_type		= "t2.micro"
+
+	tags {
+		"Name"		= "TerraForm Node X"
+	}
 }
