3b7523a1f085630ecfd37d3a5c27e6d5bf289169
Georgios Fisaris <gfisaris@gmail.com>
2016-08-13 20:11:06 +0100

    Create basic variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key" {
+    decscription = "AWS Access Key"
+}
+
+variable "aws_secret_key" {
+    description = "AWS Secret Key"
+}
+
+variable "aws_region" {
+    description = "Which AWS Region will be used ?"
+}
