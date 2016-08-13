b6e89cf3a59b3e775044aa1fefc331a0fbb8e51d
Georgios Fisaris <gfisaris@gmail.com>
2016-08-13 20:48:43 +0100

    Misspelling "resource variables must be three parts: TYPE.NAME.ATTR in:  ${aws_key_pair.gfisaris_key_name}"

--- aws.ec2-instances.tf
+++ aws.ec2-instances.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "terraform-ec2_instance-X" {
 	ami			= "ami-7abd0209"
 	instance_type		= "t2.micro"
 	
-	key_name		= "${aws_key_pair.gfisaris_key_name}"
+	key_name		= "${aws_key_pair.gfisaris.key_name}"
 
 	tags {
 		"Name"		= "TerraForm Node X"
