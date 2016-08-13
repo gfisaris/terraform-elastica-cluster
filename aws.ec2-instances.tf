08a317379321a172eb0ea3ade0bbc78c57d78284
Georgios Fisaris <gfisaris@gmail.com>
2016-08-13 21:08:01 +0100

    Associate terraform-sg-ec2-pubAccess Security Group with the EC2 Instance

--- aws.ec2-instances.tf
+++ aws.ec2-instances.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "terraform-ec2_instance-X" {
 	instance_type		= "t2.micro"
 	
 	key_name		= "${aws_key_pair.gfisaris.key_name}"
+	vpc_security_group_ids	= ["${aws_security_group.terraform-sg-ec2-pubAccess.id}"]
 
 	root_block_device {
 		volume_type		= "gp2"
