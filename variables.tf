3f7c193746573969d6b44c0bdffc07129fac6080
Georgios Fisaris <gfisaris@gmail.com>
2016-08-14 03:10:11 +0100

    Add variable key_name to be used for connecting with the EC2 Instance

--- variables.tf
+++ variables.tf
@@ -11,6 +11,10 @@ variable "aws_region" {
     default = "eu-west-1"
 }
 
+variable "key_name" {
+    description = "Name of the SSH keypair to use in AWS."
+}
+
 variable "key_path" {
     description = "Path to the private portion of the SSH key specified."
 }
