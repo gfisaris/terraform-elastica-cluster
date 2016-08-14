82951854e5efa7a400e02e3b3f77493f5d730f09
Georgios Fisaris <gfisaris@gmail.com>
2016-08-14 01:05:17 +0100

    Add VPC Security Group for ELB Public Access Control

--- aws.vpc-securitygroups.tf
+++ aws.vpc-securitygroups.tf
@@ -16,3 +16,22 @@ resource "aws_security_group" "terraform-sg-ec2-pubAccess" {
 		cidr_blocks = ["0.0.0.0/0"]
 	}
 }
+
+resource "aws_security_group" "sg-ec2-elb-pubAccess" {
+	name = "VPC:SG:EC2:ELB:PublicAccess"
+	description = "Control Public Access to EC2-ELB"
+
+	ingress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+}
