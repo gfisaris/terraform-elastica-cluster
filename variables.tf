d2e6fe5217d9d5bbb17a729509a47e90be526607
Georgios Fisaris <gfisaris@gmail.com>
2016-08-14 02:59:30 +0100

    Add all available CentOS 7 AMIs for all AWS Regions

--- variables.tf
+++ variables.tf
@@ -19,3 +19,19 @@ variable "myip_cidr_block" {
     description = "Your CIDR from which you wish to access the ElasticSearch Cluster."
     default = "0.0.0.0/0"
 }
+
+variable "aws_amis" {
+  default = {
+    "eu-west-1"         = "ami-7abd0209"
+    "eu-central-1"      = "ami-9bf712f4"
+    "us-east-1"         = "ami-6d1c2007"
+    "us-west-1"         = "ami-af4333cf"
+    "us-west-2"         = "ami-d2c924b2"
+    "sa-east-1"         = "ami-26b93b4a"
+    "ap-south-1"        = "ami-95cda6fa"
+    "ap-northeast-1"    = "ami-eec1c380"
+    "ap-northeast-2"    = "ami-c74789a9"
+    "ap-southeast-1"    = "ami-f068a193"
+    "ap-southeast-2"    = "ami-fedafc9d"
+  }
+}
