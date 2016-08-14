356a51fcfe3acc3883b7f3a86462dfd2f6c52e56
Georgios Fisaris <gfisaris@gmail.com>
2016-08-14 01:37:24 +0100

    Create extra SecurityGroup for Elastica ELBs and grant access to all Elastica Instances

--- aws.vpc-securitygroups.tf
+++ aws.vpc-securitygroups.tf
@@ -1,12 +1,12 @@
-resource "aws_security_group" "terraform-sg-ec2-pubAccess" {
-	name = "VPC:SG:EC2:PublicAccess"
-	description = "Control Public Access to Terraform created EC2 Instances"
+resource "aws_security_group" "sg-ec2-instances-elastica" {
+	name = "VPC:SG:EC2:Instances:ElasticaNodes"
+	description = "Security Group attached to Elastica EC2 Instances"
 
 	ingress {
-		from_port = 0
-		to_port = 0
-		protocol = "-1"
-		cidr_blocks = ["0.0.0.0/0"]
+		from_port = 9200
+		to_port = 9200
+		protocol = "http"
+		security_groups = [${aws_security_group.sg-ec2-elb-elastica.id}]
 	}
 
 	egress {
@@ -34,4 +34,9 @@ resource "aws_security_group" "sg-ec2-elb-pubAccess" {
 		protocol = "-1"
 		cidr_blocks = ["0.0.0.0/0"]
 	}
+}
+
+resource "aws_security_group" "sg-ec2-elb-elastica" {
+	name = "VPC:SG:EC2:ELB:Elastica"
+	description = "Security Group attached to Elastica EC2 ELBs"
 }
