c5554a1d1a9c78194614221a00a65377ff6f4e01
Georgios Fisaris <gfisaris@gmail.com>
2016-08-14 03:16:20 +0100

    Updating Key_Name

--- aws.ec2-instances.tf
+++ aws.ec2-instances.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "terraform-ec2_instance-X" {
 	ami		= "${lookup(var.aws_amis, var.aws_region)}"
 	instance_type	= "t2.micro"
 	
-	key_name		= "${aws_key_pair.gfisaris.key_name}"
+	key_name		= "${aws_key_pair.elasticaEC2Admin.key_name}"
 	vpc_security_group_ids	= ["${aws_security_group.sg-ec2-instances-elastica.id}"]
 
 	root_block_device {
