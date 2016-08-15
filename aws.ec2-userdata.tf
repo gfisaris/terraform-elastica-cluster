89c70e28df25c500b0c772f8d3ec1b056de7c604
Georgios Fisaris <gfisaris@bridgemanimages.com>
2016-08-15 23:14:42 +0100

    Removed AWS IAM credentials from ElasticSearch configuration

--- aws.ec2-userdata.tf
+++ aws.ec2-userdata.tf
@@ -2,8 +2,6 @@ resource "template_file" "elastica-cluster" {
   template = "${file("${path.module}/aws.ec2-userdata.txt")}"
 
   vars {
-    aws_access_key = "${var.aws_access_key}"
-    aws_secret_key = "${var.aws_secret_key}"
     aws_region = "${var.aws_region}"
     aws_vpc_security_groups = "${aws_security_group.sg-ec2-instances-elastica.id}"
     es_cluster_name = "${var.es_cluster_name}"
